c5f7eb15e7832de6611dcb208672b87c698b768e
Petteri Havia <petsku15@hotmail.fi>
2023-07-28 08:42:53 +0300

    Create task from input

--- src/components/CreateTask.js
+++ src/components/CreateTask.js
@@ -1,10 +1,23 @@
 import { TaskContainer, TaskButton} from "./styles/style";
+import { useState } from "react";
 
-const CreateTask = () => {
+const CreateTask = ({input, setInput, tasks, setTasks}) => {
+
+    const [message, setMessage] = useState("Create a new task...");
+
+    const handleInputValue = (e) => {
+        const value = e.target.value;
+        setInput(value);
+    }
+
+    const handleCreateTask = () => {
+        setTasks((prevTasks) => [...prevTasks, {text: input}]);
+    }
+
     return(
         <TaskContainer>
-            <input type="text" placeholder="Create a new task..."/>
-            <TaskButton>x</TaskButton>
+            <input type="text" onChange={handleInputValue} placeholder={message}/>
+            <TaskButton onClick={handleCreateTask}>x</TaskButton>
         </TaskContainer>
     )
 }
