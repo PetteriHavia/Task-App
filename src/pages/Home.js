e4c4c5d0680f9fd687034ce0071e1210b0664874
Petteri Havia <petsku15@hotmail.fi>
2023-07-28 08:47:36 +0300

    States that manage input and tasks

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,18 @@
+import { useState } from 'react';
 import {Container} from '../components/styles/style.js';
 import styled from "styled-components";
 import CreateTask from '../components/CreateTask.js';
 import TaskList from '../components/TaskList.js'
 
-
 const Home = () => {
+
+    const [input, setInput] = useState("");
+    const [tasks, setTasks] = useState([]);
+
     return(
         <Container>
-            <CreateTask />
-            <TaskList />
+            <CreateTask input={input} setInput={setInput} setTasks={setTasks} tasks={tasks}/>
+            <TaskList input={input} setInput={setInput} setTasks={setTasks} tasks={tasks}/>
         </Container>
     )
 }
