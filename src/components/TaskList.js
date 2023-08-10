0358a4b4c54414650eaf090895e47201164ec20d
Petteri Havia <petsku15@hotmail.fi>
2023-08-10 18:25:51 +0300

    Add remove all completed tasks function + show active tasks count

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,19 +1,34 @@
-import {ListContainer, StatusBar} from './styles/style.js'
-import Tabs from './Tabs.js';
+import { ListContainer, StatusBar } from "./styles/style.js";
+import { useState } from "react";
+import Tabs from "./Tabs.js";
 
-const TaskList = ({tasks, setTasks}) => {
+const TaskList = ({ tasks, setTasks }) => {
+  const [filterTask, setFilterTask] = useState([]);
 
-    return(
-        <ListContainer>
-            <Tabs tasks={tasks} setTasks={setTasks}/>
-            <StatusBar>
-                <div className="active-tasks">
-                    <h4>0 Active</h4>
-                </div>
-                <button>Clear Completed</button>
-            </StatusBar>
-        </ListContainer>
-    )
-}
+  const activeCount = tasks.length - filterTask.length;
+
+  const handleClearCompleted = () => {
+    const activeTasks = tasks.filter((task) => !filterTask.includes(task.text));
+    setTasks(activeTasks);
+    setFilterTask([]);
+  };
+
+  return (
+    <ListContainer>
+      <Tabs
+        tasks={tasks}
+        setTasks={setTasks}
+        filterTask={filterTask}
+        setFilterTask={setFilterTask}
+      />
+      <StatusBar>
+        <div className="active-tasks">
+          <h4>{activeCount} Active</h4>
+        </div>
+        <button onClick={handleClearCompleted}>Clear Completed</button>
+      </StatusBar>
+    </ListContainer>
+  );
+};
 
 export default TaskList;
